369363632ecff5026700963c24d69dfcef742bb4
Song, Jae Sung <fegeterika@gmail.com>
2018-01-10 12:55:06 -0500

    Create 0118.Pascals_Triangle.java

---
+++ Algorithm_Practices/0118.Pascals_Triangle.java
@@ -0,0 +1,31 @@
+/*
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<Integer>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    temp.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+}

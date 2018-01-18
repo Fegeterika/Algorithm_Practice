ce25053fc87ace46566287e804da58ebce3810bf
Song, Jae Sung <fegeterika@gmail.com>
2018-01-18 12:47:08 -0500

    Create 0046.Permutations.java

---
+++ Algorithm_Practices/0046.Permutations.java
@@ -0,0 +1,35 @@
+/*
+Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), nums);
+        return res;
+    }
+    
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (temp.contains(nums[i])) continue;
+                temp.add(nums[i]);
+                backtrack(res, temp, nums);
+                temp.remove(temp.size() - 1);
+            }
+        }
+        
+    }
+}

6266a916d8ff3d59e9facbd4ea0b279aa25a0330
Song, Jae Sung <fegeterika@gmail.com>
2018-01-19 12:06:07 -0500

    Update 0028.Implement_strStr.java

--- Algorithm_Practices/0028.Implement_strStr.java
+++ Algorithm_Practices/0028.Implement_strStr.java
@@ -12,6 +12,20 @@ Example 2:
 Input: haystack = "aaaaa", needle = "bba"
 Output: -1
 */
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("")) return 0;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            for (int j = 0; j < needle.length() && haystack.charAt(i + j) == needle.charAt(j); j++) {
+                if (j == needle.length() - 1) return i;
+            }
+        }
+        return -1;
+    }
+}
+
+/*
 class Solution {
     public int strStr(String haystack, String needle) {
         if (needle.equals("")) return 0;
@@ -28,3 +42,4 @@ class Solution {
         return -1;
     }
 }
+*/

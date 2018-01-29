62f512750a9eefa15549ef84b8d5f3af41740640
Song, Jae Sung <fegeterika@gmail.com>
2018-01-29 16:14:05 -0500

    Update 0066.Plus_One.java

--- Algorithm_Practices/0066.Plus_One.java
+++ Algorithm_Practices/0066.Plus_One.java
@@ -5,6 +5,24 @@ You may assume the integer do not contain any leading zero, except the number 0 
 
 The digits are stored such that the most significant digit is at the head of the list.
 */
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] resForAllNines = new int[digits.length + 1];
+        resForAllNines[0] = 1;
+        return resForAllNines;
+    }
+}
+
+/*
 class Solution {
     public int[] plusOne(int[] digits) {
         int[] res = new int[digits.length + 1];
@@ -28,3 +46,4 @@ class Solution {
         }
     }
 }
+*/

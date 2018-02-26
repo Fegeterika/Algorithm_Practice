00be299d770d9d51c456b1d92e8e833e3ecc5366
Song, Jae Sung <fegeterika@gmail.com>
2018-02-25 21:25:16 -0500

    Create 0169.Majority_Element.java

---
+++ Algorithm_Practices/0169.Majority_Element.java
@@ -0,0 +1,15 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+*/
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
+            if (cnt.get(num) > nums.length / 2) return num;
+        }
+        return 0;
+    }
+}

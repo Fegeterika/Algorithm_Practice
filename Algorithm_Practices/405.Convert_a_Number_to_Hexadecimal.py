a90df81b5a0508aefde909dca33c864f16ea3ed3
Song, Jae Sung <fegeterika@gmail.com>
2018-03-05 11:23:54 -0500

    Create 405.Convert_a_Number_to_Hexadecimal.py

---
+++ Algorithm_Practices/405.Convert_a_Number_to_Hexadecimal.py
@@ -0,0 +1,35 @@
+"""
+Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+Note:
+
+All letters in hexadecimal (a-f) must be in lowercase.
+The hexadecimal string must not contain extra leading 0s. If the number is zero, it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal string will not be the zero character.
+The given number is guaranteed to fit within the range of a 32-bit signed integer.
+You must not use any method provided by the library which converts/formats the number to hex directly.
+Example 1:
+
+Input:
+26
+
+Output:
+"1a"
+Example 2:
+
+Input:
+-1
+
+Output:
+"ffffffff"
+"""
+class Solution:
+    def toHex(self, num):
+        if num == 0:
+            return '0'
+        res = ''
+        mapping = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+        for i in range(8):
+            res = mapping[num & 15] + res
+            num = num >> 4
+        return res.lstrip('0')
+        
